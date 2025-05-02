253a524ea164b6f43e44dc743e21df8c507d1cc0
Colle <colleluizz@gmail.com>
2025-05-01 21:40:31 -0300

    Add files via upload

---
+++ Strategy/src/Main.java
@@ -0,0 +1,19 @@
+import model.Disciplina;
+import strategy.Aritmetica;
+import strategy.Geometrica;
+import strategy.MediaStrategy;
+
+public class Main {
+    public static void main(String[] args) {
+        MediaStrategy estrategia = new Aritmetica();  // Troque por: new Geometrica()
+        Disciplina d = new Disciplina(estrategia);
+
+        d.setNome("Padrões de Desenvolvimento");
+        d.setP1(10);
+        d.setP2(5);
+        d.calcularMedia();
+
+        System.out.printf("P1: %.2f  P2: %.2f  Média: %.2f  Situação: %s%n",
+                d.getP1(), d.getP2(), d.getMedia(), d.getSituacao());
+    }
+}
